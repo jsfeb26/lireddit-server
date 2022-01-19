5b1a7edb29d8d50475455cc6ff16dd7888ecaee2
Jason Stinson <jsfeb26@gmail.com>
2022-01-19 10:09:06 -0600

    Move sleep into its own util function

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -3,13 +3,10 @@ import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
 
-const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
-
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
-    await sleep(3000);
     return em.find(Post, {});
   }
 
