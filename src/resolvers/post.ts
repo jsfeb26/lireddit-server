43353294b0814631d49c9ec8f4bd36f1b946afca
Jason Stinson <jsfeb26@gmail.com>
2022-01-18 18:08:43 -0600

    Add artificial delay to posts query

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -3,10 +3,13 @@ import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
 
+const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+  async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    await sleep(3000);
     return em.find(Post, {});
   }
 
